0d038aab487b61a7fe06dfbe6aa8536a82b4adef
andy <andy@3e7f758e-98ca-4f56-a62f-92ae06c0b43c>
2007-10-01 23:18:00 +0000

    restructured some material without changing meaning or function

--- mv1.py
+++ mv1.py
@@ -4,75 +4,7 @@ mv1.py unrealistic code for small simple models
 """
 import numpy, scipy, scipy.linalg, random, math
 
-from itertools import izip
-argmax = lambda array: max(izip(array, xrange(len(array))))[1] # Daniel Lemire
-
-def index_to_permute(i,  # Index in range [0,n!-1]
-                     n   # Number of items
-                     ):  # Returns array of n items describing the permutation
-    outs = []
-    for k in xrange(2,n):
-        j = i%k
-        i /= k
-        outs.append(j)
-    outs.append(i)
-    v = []
-    ins = range(n)
-    for k in xrange(n-1,0,-1):
-        v = v + [ins.pop(outs.pop())]
-    return v + ins
-
-def permute_to_index(v):
-    order = []
-    i = 0
-    while len(v) > 1:
-        F = v.pop(0)
-        c = 0
-        for t in v:
-            if t < F:
-                c += 1
-        i *= len(v)+1
-        i +=c
-    return i
-
-def column(a):
-    """
-    A utility that forces argument 'a' to be numpy column vector.
-    """
-    m = numpy.mat(a)
-    r,c = m.shape
-    if min(r,c) > 1:
-        raise RuntimeError,"(r,c)=(%d,%d)"%(r,c)
-    if c == 1:
-        return m
-    else:
-        return m.T
-
-def normalS(mu,sigma,C=None):
-    """
-    A utility that returns a sample from a vector normal distribtuion.
-    """
-    if C == None:
-        C = numpy.mat(scipy.linalg.cholesky(sigma))
-    r,c = C.shape  # rows and columns
-    z = []
-    for i in xrange(r):
-        z.append(random.gauss(0,1))
-    z =  numpy.matrix(z)
-    return column(mu) + C*z.T
-    
-def normalP(mu,sigma, x):
-    """
-    A utility that returns the density of N(mu,sigma) at x.
-    """
-    from scipy.linalg import solve, det
-    r,c = sigma.shape
-    d = x-mu
-    Q = d.T*solve(sigma,d)
-    D = det(sigma)
-    norm = 1/((2*math.pi)**(r/2.0)*D)
-    P = norm*math.exp(-Q)
-    return P
+import util
 
 class MV1:
     """A simple model of observed motion with the following groups of
@@ -117,7 +49,7 @@ class MV1:
         states."""
         s_j = []
         for j in xrange(self.N_obj):
-            s_j.append(normalS(self.mu_init,self.Sigma_init))
+            s_j.append(util.normalS(self.mu_init,self.Sigma_init))
 
         s_dim = self.mu_init.shape[1]
         zero_s = scipy.matrix(scipy.zeros(s_dim))
@@ -131,12 +63,12 @@ class MV1:
                 i = random.randint(0,self.N_perm-1)
             else:
                 i = 0 # No shuffle for t=0
-            permute = index_to_permute(i,self.N_obj)
+            permute = util.index_to_permute(i,self.N_obj)
             obs_t = range(self.N_obj) # Make list with length N_obj
             state_t = []
             for j in xrange(self.N_obj):
-                epsilon = normalS(zero_s,self.Sigma_D) # Dynamical noise
-                eta = normalS(zero_y,self.Sigma_O) # Observational noise
+                epsilon = util.normalS(zero_s,self.Sigma_D) # Dynamical noise
+                eta = util.normalS(zero_y,self.Sigma_O) # Observational noise
                 s_t = self.A*s_j[j] + epsilon
                 y_t = self.O * s_t + eta
                 s_j[j] = s_t
@@ -207,7 +139,7 @@ class MV1:
         perm_list = []
         for p in xrange(self.N_perm):
             perm = {}
-            perm['p_vector'] = index_to_permute(p,self.N_obj)
+            perm['p_vector'] = util.index_to_permute(p,self.N_obj)
             perm['B_p'] = []  # List of best predecessor permutations
             perm['nu_s'] = [] # List of utlity of best path to s
             for t in xrange(T):
---
+++ util.py
@@ -0,0 +1,80 @@
+"""
+util.py utilities for persistent surveillance tracking
+
+"""
+import numpy, scipy, scipy.linalg, random, math
+
+from itertools import izip
+argmax = lambda array: max(izip(array, xrange(len(array))))[1] # Daniel Lemire
+
+def index_to_permute(i,  # Index in range [0,n!-1]
+                     n   # Number of items
+                     ):  # Returns array of n items describing the permutation
+    outs = []
+    for k in xrange(2,n):
+        j = i%k
+        i /= k
+        outs.append(j)
+    outs.append(i)
+    v = []
+    ins = range(n)
+    for k in xrange(n-1,0,-1):
+        v = v + [ins.pop(outs.pop())]
+    return v + ins
+
+def permute_to_index(v):
+    order = []
+    i = 0
+    while len(v) > 1:
+        F = v.pop(0)
+        c = 0
+        for t in v:
+            if t < F:
+                c += 1
+        i *= len(v)+1
+        i +=c
+    return i
+
+def column(a):
+    """
+    A utility that forces argument 'a' to be numpy column vector.
+    """
+    m = numpy.mat(a)
+    r,c = m.shape
+    if min(r,c) > 1:
+        raise RuntimeError,"(r,c)=(%d,%d)"%(r,c)
+    if c == 1:
+        return m
+    else:
+        return m.T
+
+def normalS(mu,sigma,C=None):
+    """
+    A utility that returns a sample from a vector normal distribtuion.
+    """
+    if C == None:
+        C = numpy.mat(scipy.linalg.cholesky(sigma))
+    r,c = C.shape  # rows and columns
+    z = []
+    for i in xrange(r):
+        z.append(random.gauss(0,1))
+    z =  numpy.matrix(z)
+    return column(mu) + C*z.T
+    
+def normalP(mu,sigma, x):
+    """
+    A utility that returns the density of N(mu,sigma) at x.
+    """
+    from scipy.linalg import solve, det
+    r,c = sigma.shape
+    d = x-mu
+    Q = d.T*solve(sigma,d)
+    D = det(sigma)
+    norm = 1/((2*math.pi)**(r/2.0)*D)
+    P = norm*math.exp(-Q)
+    return P
+            
+#---------------
+# Local Variables:
+# eval: (python-mode)
+# End:
