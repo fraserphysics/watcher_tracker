00d961d5bf86db9079ff8de33e2b3b3c068ea21d
andy <andy@3e7f758e-98ca-4f56-a62f-92ae06c0b43c>
2007-10-11 04:18:26 +0000

    Explained forward step for y=None in model.tex.  Go mv2.py to run, but wrong

--- mv2.py
+++ mv2.py
@@ -15,34 +15,32 @@ class TARGET(mv1a.TARGET):
         Create a new target with updated m_t, mu_t, Sigma_t and
         R_t for the observation, index pair (y,m).  This is the second
         half of Kalman filtering step."""
-        if m_L[-1] is -1:
-            v_old = 1
-        else:
-            v_old = 0
-        if m is -1:
-            v_new = 1
-        else:
-            v_new = 0
-        Delta_R = math.log(self.mod.PV_V[v_old,v_new])
+        vv = [self.m_L[-1],m]
+        for k in [0,1]:
+            if vv[k] is -1:
+                vv[k] = 1
+            else:
+                vv[k] = 0
+        Delta_R = math.log(self.mod.PV_V[vv[0],vv[1]])
         m_L = self.m_t+[m]
         if y is None:
-            mu_L = self.mu_t + [self.mu_a]
             Sigma_L = self.Sigma_t + [self.Sigma_a]
-            Delta_R += log_det_stuff
+            mu_L = self.mu_t + [self.mu_a]
+            Delta_R -= self.mod.log_det_Sig_D/2
         else:
             Delta_y = y - self.y_forecast    # Error of forecast observation
             Sigma_L = self.Sigma_t + [self.Sigma_next]
             mu_L = self.mu_t + [self.mu_a + self.K*Delta_y]
-            Delta_R += log_det_stuff  - float(
-                Delta_y.T*self.Sigma_y_forecast_I*Delta_y)/2
+            Delta_R -= (self.mod.log_det_Sig_D+self.mod.log_det_Sig_O + float(
+                Delta_y.T*self.Sigma_y_forecast_I*Delta_y))/2
         R_L = self.R_t + [self.R_t[-1] + Delta_R]
         
         return TARGET(self.mod,m_L,mu_L,Sigma_L,R_L)
 
 class PERMUTATION(mv1a.PERMUTATION):
-    def make_children(self, # self is a PERMUTATION
-                      y_t,  # All observations at time t
-                      cousins
+    def make_children(self,   # self is a PERMUTATION
+                      y_t,    # All observations at time t
+                      cousins # Targets from all permutations
                       ):
         for target in self.targets:
             target.make_children(y_t,cousins)
@@ -51,45 +49,55 @@ class PERMUTATION(mv1a.PERMUTATION):
                 cousins[key] = target.update(None,-1)
             target.children[-1] = cousins[key]
 
-class MV2(mv1a.MV1A):
-    def __init__(self,PV_V=[[1,0],[0,1]],**kwargs)
-    mv1a.MV1A.__init__(self,**kwargs)
-    self.PV_V = scipy.matrix(PV_V)
+class MV2(mv1a.MV1a):
+    def __init__(self,PV_V=[[1,0],[0,1]],**kwargs):
+        mv1a.MV1a.__init__(self,**kwargs)
+        self.PV_V = scipy.matrix(PV_V)
+        self.log_det_Sig_D = scipy.linalg.det(self.Sigma_D)
+        self.log_det_Sig_O = scipy.linalg.det(self.Sigma_O)
 
     
     def simulate(self, T):
         """ Return a sequence of T observations and a sequence of T
         states."""
-        s_j = []
+        x_j = []
+        v_j = []
         for j in xrange(self.N_tar):
-            s_j.append(util.normalS(self.mu_init,self.Sigma_init))
+            x_j.append(util.normalS(self.mu_init,self.Sigma_init))
+            v_j.append(0) # Start with all trajectories visible
 
-        s_dim = self.mu_init.shape[1]
-        zero_s = scipy.matrix(scipy.zeros(s_dim))
+        x_dim = self.mu_init.shape[1]
+        zero_x = scipy.matrix(scipy.zeros(x_dim))
         y_dim = self.Sigma_O.shape[0]
         zero_y = scipy.matrix(scipy.zeros(y_dim))
         
         obs = []
-        states = []
+        xs = []
+        vs = []
         for t in xrange(T):
             if t>0:
                 i = random.randint(0,self.N_perm-1)
             else:
                 i = 0 # No shuffle for t=0
             permute = util.index_to_permute(i,self.N_tar)
-            obs_t = range(self.N_tar) # Make list with length N_tar
             state_t = []
             for j in xrange(self.N_tar):
-                epsilon = util.normalS(zero_s,self.Sigma_D) # Dynamical noise
+                pv = self.PV_V[v_j[j],0]
+                if pv > random.random():
+                    v_j[j] = 0
+                else:
+                    v_j[j] = 1
+                epsilon = util.normalS(zero_x,self.Sigma_D) # Dynamical noise
+                x_j[j] = self.A*x_j[j] + epsilon
+            obs_t = []
+            for j in xrange(self.N_tar):
+                if v_j[j] is not 0:
+                    continue
                 eta = util.normalS(zero_y,self.Sigma_O) # Observational noise
-                s_t = self.A*s_j[j] + epsilon
-                y_t = self.O * s_t + eta
-                s_j[j] = s_t
-                obs_t[permute[j]] = y_t
-                state_t.append(s_t)
+                obs_t.append(self.O * x_j[permute[j]] + eta)
             obs.append(obs_t)
-            states.append(state_t)
-        return obs,states
+            xs.append(x_j)
+        return obs,xs
 
 # Test code
 if __name__ == '__main__':
