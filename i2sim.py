af86e473904db58cfb05cdfab86613502a5abbe0
andy <andy@3e7f758e-98ca-4f56-a62f-92ae06c0b43c>
2011-04-24 01:56:13 +0000

    Seems satisfactory now

--- i2sim.py
+++ i2sim.py
@@ -1,82 +1,40 @@
 import numpy, random, fltk
 
-class my_image:
-    """ Image with metadata describing how it should appear.
-    """
-    def __init__(self,A,exp=1,top=None,bottom=None):
-        self.A = A
-        W,H,three = A.shape
-        assert three == 3
-        if top == None:
-            self.top = numpy.ones(3)*A.max()
-        else:
-            self.top = top
-        if bottom == None:
-            self.bottom = numpy.ones(3)*A.min()
-        else:
-            self.bottom = bottom
-        self.scale = 1.0/(self.top-self.bottom)
-        self.exp = exp
-        return
-    def display(self):
-        A = numpy.minimum(((self.A-self.bottom)*self.scale)**self.exp,1)*255.0
-        return numpy.array(A.transpose((1,0,2))[::-1,:,:],numpy.uint8)
-    def float_line(self,line):
-        """ Return values in 'line' mapped to interval [0,1)
-        """
-        return numpy.minimum(((line-self.bottom)*self.scale)**self.exp,1)
-
 class FltkCanvas(fltk.Fl_Widget):
     """ A widget that has an array of type uint8 in self._image.  For
-    displaying images
+    displaying images.  Variant of class in pushbroom/utilities.py
     """
     def __init__(self, #FltkCanvas
                  x,y,w,h,A):
         fltk.Fl_Widget.__init__(self, x, y, w, h, "canvas")
-        self._oldsize=(None,None)
         self._draw_overlay = False
         self._button = None
         self._key = None
         self.new_image(A)
     def new_image(self, #FltkCanvas
                  A,MAX=None):
-        if isinstance(A,my_image):
-            self._image = A.display()
-            return
-        if A.ndim == 3:
-            W,H,D = A.shape
         if A.ndim == 2:
             W,H = A.shape
             D = 1
             A = A.reshape(W,H,D)
-        if A.dtype == numpy.dtype(numpy.bool):
-            A = numpy.array(A*255,numpy.uint8)
-        if A.dtype != numpy.dtype(numpy.uint8):
-            if MAX == None:
-                MAX = A.max(0).max(0)
-            MIN = A.min(0).min(0)
-            scale = 255.0/(MAX-MIN)
-            A = numpy.array((A-MIN)*scale,numpy.uint8)
-	self._image=A.transpose((1,0,2))[::-1,:,:].copy()
-        # Transpose (W,H,D) => (H,W,D) for fltk drawing.  ::-1 to put
-        # origin in lower left copy() to make buffer interface work
+	self._image=A.copy()
         return
     def draw(self #FltkCanvas
                  ):
         newsize=(self.w(),self.h())
-        if(self._oldsize !=newsize):
-            self._oldsize =newsize
         H,W,D = self._image.shape # fl_draw_image assumes different axes order
         fltk.fl_draw_image(self._image,self.x(),self.y(),W,H,D,0)
         self.redraw()
         return
 
 class REGION(object):
-    def __init__(self,_pass,period,n,limits):
-        self._pass = _pass
-        self.period = period
-        self.n = n
-        self.limits = limits
+    """ A component of the geometry of a simulation.
+    """
+    def __init__(self,_pass,period,limits):
+        self._pass = _pass    # True/False Is passing allowed?
+        self.period = period  # Period of cycle of sequence of speed limits 
+        self.limits = limits  # Array of limits
+        self.n = len(limits)  # Number of different speed limits
         return
     def limit(self,t):
         return self.limits[int(self.n*t/self.period)%self.n]
@@ -87,16 +45,16 @@ class GEOMETRY(object):
     Describes road
     """
     time = 0
-    boundaries = numpy.array([0,100,200,300],numpy.float64)
+    boundaries = numpy.array([0,200,400,600],numpy.float64)
     regions = {0:REGION(False,     # No passing
-                        1,1,[2]    # One speed limit of 2.0
+                        1,[2]      # One speed limit of 2.0
                         ),
                1:REGION(True,      # Passing OK
-                        50,2,[2,1] # Speed limit switches between 2
-                                   # and 1 with period 50
+                        300,[4,4,1]# Speed limit switches between 4
+                                   # and 1.  300 steps per interval
                         ),
                2:REGION(True,      # Passing OK
-                        1,1,[3])   # Speed limit is always 3
+                        1,[2])     # Speed limit is always 8
                }
     def __init__(self):
         return
@@ -106,63 +64,80 @@ class GEOMETRY(object):
         """
         assert pos >= self.boundaries[0]
         for r in xrange(len(self.boundaries)-1):
-            # For many regions should avoid this linear search
+            # If many regions, need code to avoid this linear search
             if pos < self.boundaries[r+1]:
                 return r
         return r
     def limit(self,pos,t):
+        """ Return speed limit for pos and t
+        """
         return self.regions[self.region(pos)].limit(t)
     def _Pass(self,pos):
         return self.regions[self.region(pos)]._Pass()
     def bounds(self,pos):
+        """ Check if pos is in the boundaries defined by self
+        """
         if pos < self.boundaries[0] or pos > self.boundaries[-1]:
             return False
         return True
 class TARGET(object):
     """
-    A class whose instances have invariant characteristics and
-    position and velocity
+    A class whose instances have constant characteristics and position
+    and velocity
     """
-    accel_var = 0.01
-    close = 1.0
-    speed_dev = 0.2  # Deviation of preferred speed relative to limit
-    relax = 0.05
-    def __init__(self,G):
+    def __init__(self,G,accel_var,close,speed_var,relax):
         self.G = G
+        self.accel_var = accel_var
+        self.close = close
+        self.relax = relax
         self.pos = 0.0
-        self.albedo = random.random()
-        self.mean = random.gauss(1,TARGET.speed_dev**2) # Preferred rvel
-        self.rvel = random.gauss(self.mean,TARGET.accel_var/TARGET.relax)
+        albedo = numpy.array([random.randint(1,255) for x in xrange(3)],
+                             numpy.int16)
+        albedo *= 512.0/albedo.sum()
+        self.albedo = albedo
+        self.mean = max(.1,random.gauss(1,speed_var)) # Preferred rvel
+        self.rvel = max(.1,random.gauss(self.mean,accel_var/relax))
+        # rvel, the "relative velocity", is the fraction of the speed
+        # limit this target tries to use
         return
     def step(self, t, other):
         old_limit = self.G.limit(self.pos,t)
-        self.rvel = random.gauss(0,TARGET.accel_var) + TARGET.relax*self.mean +(
-            1-TARGET.relax)*self.rvel
+        self.rvel = max(.1, random.gauss(0,self.accel_var) +
+                         self.relax*self.mean + (1-self.relax)*self.rvel)
         self.vel = old_limit*self.rvel
         self.pos += self.vel
-        if (other != None and
-            (not self.G._Pass(self.pos)) and
-            (other.pos + TARGET.close < self.pos)):
-            #If passing forbidden and next target too close
-            self.pos = other.pos - TARGET.close
+        #Follow leader if passing forbidden and next target too close
+        if (other != None and (not self.G._Pass(self.pos)) and
+                (other.pos < self.close + self.pos)):
+            self.pos = max(.1,other.pos - self.close)
             self.vel = other.vel
         new_limit = self.G.limit(self.pos,t)
         self.rvel = self.vel/new_limit
         return self.G.bounds(self.pos)
 
 class animator(object):
-    def __init__(self,canvas,G):
+    """ Keeps track of targets and pixels in display
+    """
+    def __init__(self,canvas,G,sliders):
         self.canvas = canvas
         self.t = 0
         self.G = G
+        self.sliders = sliders
         self.targets = []
         return
     def update(self,ptr):
         import time
-        A = self.canvas._image
-        if self.t%5 == 0:
-            self.targets.append(TARGET(self.G))
-        self.targets.sort(key=operator.attrgetter('pos'))
+        
+        rate = self.sliders['rate']['value']
+        accel_var = self.sliders['accel_dev']['value']**2
+        speed_var = self.sliders['speed_dev']['value']**2
+        relax = self.sliders['relax']['value']
+        delay = self.sliders['delay']['value']
+        close = 5.0
+
+        if random.random() < rate:
+            self.targets.append(TARGET(self.G,accel_var,close,speed_var,relax))
+        self.targets.sort(key=operator.attrgetter('pos'),reverse=False)
         for i in xrange(len(self.targets)-1,-1,-1):
             if i == len(self.targets)-1:
                 other = None
@@ -172,16 +147,18 @@ class animator(object):
                 pass
             else:
                 del self.targets[i]
+        A = self.canvas._image
         X,Y,D = A.shape
-        A[1:,:,:] = A[:-1,:,:]
-        A[0,:,0] *= 0
+        B = 20 # Width of upper band
+        A[B:,:,:] = A[(B-1):-1,:,:]
+        A[0:B,:,:] *= 0
         for target in self.targets:
             x = int(target.pos)
             #A[x,y,0] = int(target.albedo)%256
-            A[0,x,0] = 255
+            A[0:B,max(0,x-close):x,:] = target.albedo
         self.canvas.redraw()
         self.t += 1
-        time.sleep(0.02)
+        time.sleep(delay)
         return    
 
 if __name__ == '__main__': # Test code
@@ -189,7 +166,7 @@ if __name__ == '__main__': # Test code
     def slider_cb(slider,args): # Call back for sliders
         global slide_dict
         key = args[0]
-        slide_dict[key]['value'] = int(slider.value())
+        slide_dict[key]['value'] = slider.value()
         for act in args[1]:
             act()
         return
@@ -197,10 +174,12 @@ if __name__ == '__main__': # Test code
     keys = [
         'key',      'value','min','max','step','acts']
     slide_list = [
-        ['rate',      0.01,  0,   .1,    0.001, []],
+        ['rate',      0.03,  0,   .5,    0.01, []],
         ['accel_dev', 0.03,  0,   .1,    0.001, []],
-        ['speed_dev', 0.2,   0,   .2,    0.002, []],
-        ['relax',     0.05,  0,   .1,    0.001, []]]
+        ['speed_dev', 0.2,   0,   .5,    0.002, []],
+        ['relax',     0.05, .001, .5,    0.001, []],
+        ['delay',     0.02,  0,   .5,    0.005, []]
+        ]
     slide_dict = {}
     for slide in slide_list:
         t_dict = {}
@@ -214,8 +193,8 @@ if __name__ == '__main__': # Test code
     R_HEIGHT= S_LENGTH# Height of each row
     VPS     = 10      # Space between elements in V_Pack
     V_SPACE = 40      # Vertical space between rows of sliders
-    WIDTH =   700     # Width of window
-    SWIDTH  = 300     # Width of slider region
+    WIDTH =   1000    # Width of window
+    SWIDTH  = 390     # Width of slider region
     H_SPACE = 20      # Horizontal space between sliders
     VPW = int((SWIDTH)/N_s - H_SPACE) # Width of V_Pack
     Y_      = 5       # Starting y position in window
@@ -249,33 +228,15 @@ if __name__ == '__main__': # Test code
     for slide in slide_list:
         H_Pack.children.append(Slide(slide[0]))
     H_Pack.end()
-    A = numpy.zeros((WIDTH-SWIDTH,HEIGHT),numpy.uint8)
-    canvas = FltkCanvas(SWIDTH,0,SWIDTH,HEIGHT,A)
+    A = numpy.zeros((HEIGHT,WIDTH-SWIDTH,3),numpy.uint8)
+    canvas = FltkCanvas(SWIDTH,0,WIDTH-SWIDTH,HEIGHT,A)
     canvas.new_image(A)
     window.end()
     window.show(len(sys.argv), sys.argv)
-    anim = animator(canvas,GEOMETRY())
+    anim = animator(canvas,GEOMETRY(),slide_dict)
     fltk.Fl.add_idle(anim.update)
     fltk.Fl.run()
     
-    # T = 1000
-    # G = GEOMETRY()
-    # targets = []
-    # for t in xrange(T):
-    #     if t%50 == 0:
-    #         targets.append(TARGET(G))
-    #     targets.sort(key=operator.attrgetter('pos'))
-    #     for i in xrange(len(targets)-1,-1,-1):
-    #         if i == len(targets)-1:
-    #             other = None
-    #         else:
-    #             other = targets[i+1]
-    #         if targets[i].step(t,other):
-    #             print('t=%3d pos[%2d]=%f'%(t,i, targets[i].pos))
-    #         else:
-    #             del targets[i]
-    #             print('targets[%d] rolled off the end'%i)
-
 #---------------
 # Local Variables:
 # eval: (python-mode)
